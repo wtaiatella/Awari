b4cc2df175a78d2176d5b7669a63cbcd1e8c2a1f
Wagner Taiatella <wtaiatella@gmail.com>
2022-04-19 15:37:52 -0400

    feat: add build release in docker-compose

--- aula-blog/index.js
+++ aula-blog/index.js
@@ -1,37 +1,98 @@
-var express = require("express");
+var express = require('express')
 
 const knex = require('knex')({
-    client: 'mysql2',
-    connection: {
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
-        user: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASS,
-        database: process.env.DATABASE_NAME
-    }
-});
+  client: 'mysql2',
+  connection: {
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
+  },
+  migrations: {
+    directory: 'migrations',
+  },
+})
 
-var server = express();
+var server = express()
 
-server.get("/posts", function(request, response) {
-    knex
-        .select()
-        .from('posts')
-        .then(function(rows) {
-            response.json(rows);
-        })
-        .catch(function(error) {
-            console.log(error);
+server.use(express.json())
+/* server.use(function (request) {
+  console.log(request.body)
+  return request
+}) */
 
-            response.status(500);
-            response.json({error: "Something went wrong"});
-        });
-});
+var teste
+server.param('id', function (req, res, next, id) {
+  teste = id
+  console.log('id = ', id)
+  next()
+})
 
-server.post("/posts", function(request, response) {
-    console.log(request.json())
+server.get('/posts', function (request, response) {
+  //console.log(request)
+  knex
+    .select()
+    .from('posts')
+    .then(function (rows) {
+      response.json(rows)
+      console.log(rows)
+    })
+    .catch(function (error) {
+      console.log(error)
 
-    response.json({message: "Hello World 2"});
-});
+      response.status(500)
+      response.json({ error: 'Something went wrong' })
+    })
+})
 
-server.listen(3000);
+server.get('/posts/:id', function (request, response) {
+  console.log('id get= ', teste)
+  knex
+    .select()
+    .from('posts')
+    .where('id', '=', teste)
+    .then(function (rows) {
+      response.json(rows)
+      console.log(rows)
+    })
+    .catch(function (error) {
+      console.log(error)
+      response.status(500)
+      response.json({ error: 'Something went wrong' })
+    })
+})
+
+server.post('/posts', function (request, response) {
+  knex('posts')
+    .insert(request.body)
+    .then(function (id) {
+      response.status(201)
+      response.json({ id: id })
+    })
+    .catch(function (error) {
+      console.log(error)
+
+      response.status(500)
+      response.json({ error: 'Something went wrong' })
+    })
+})
+
+server.put('/posts/:id', function (request, response) {
+  console.log('id = ', teste)
+  knex('posts')
+    .where('id', '=', teste)
+    .update(request.body)
+    .then(function (id) {
+      console.log(id)
+      if (id === 1) {
+        response.status(201)
+        response.json({ id: teste })
+      } else {
+        response.status(500)
+        response.json({ error: 'Something went wrong' })
+      }
+    })
+})
+
+server.listen(3000)
